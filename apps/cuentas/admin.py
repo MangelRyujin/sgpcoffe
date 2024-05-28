685eda7e57084d354c2a80f08080e1f94ec49f76
mangelryujin <mangelryujin@gmail.com>
2024-05-28 16:58:52 -0400

    denegate permision delete in CashOperationAdmin

--- apps/cuentas/admin.py
+++ apps/cuentas/admin.py
@@ -69,6 +69,9 @@ class CashOperationAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
     
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
 
 
 # # Admin Items 
